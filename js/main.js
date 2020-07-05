364c3c3d790be98cabf1de429f29804b5160c086
Farid Mammadov <fareedmammadov@gmail.com>
2020-07-05 20:05:58 +0400

    added favicon, animation to map svg, hover to sponsors, changed box shadow on social icons.

--- js/main.js
+++ js/main.js
@@ -33,3 +33,19 @@ $(".arrow-left").click(function () {
 $(".dot").click(function () {
   owl.trigger("to.owl.carousel", [$(this).index(), 300]);
 });
+
+// const locationsMap = document.getElementById("locations-map");
+// const circles = locationsMap.querySelectorAll("circle");
+
+// for (circle of circles) {
+//   circle.style.opacity = "0";
+// }
+
+$(window).on("scroll", function () {
+  scroll_pos = $(window).scrollTop() + $(window).height();
+  element_pos =
+    $("#locations-map").offset().top + $("#locations-map").height() - 100;
+  if (scroll_pos > element_pos) {
+    $("#locations-map circle").addClass("circle--active");
+  }
+});
