60eab31faed6a8a78e063d3a283bb018e86e3a0e
Farid Mammadov <fareedmammadov@gmail.com>
2020-07-06 17:05:26 +0400

    some changes on side navigation

--- js/main.js
+++ js/main.js
@@ -37,14 +37,16 @@ $(".dot").click(function () {
 
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 const smallNav = document.querySelector(".small-nav");
-const smallNavItems = document.querySelectorAll(".small-nav ul li");
+const smallNavItems = document.querySelectorAll(".small-nav ul li a");
 
 hamburgerMenu.addEventListener("click", function () {
   smallNav.classList.toggle("small-nav--active");
+  document.body.classList.toggle("overflow-h");
 });
 
 smallNavItems.forEach((item) => {
   item.addEventListener("click", () => {
     smallNav.classList.toggle("small-nav--active");
+    document.body.classList.toggle("overflow-h");
   });
 });
